d7b0dd4fd8083d8dadc94e7ba8cfe16cfefb513b
hamdan <hamdan.ali@septemsystems.com>
2023-06-13 19:25:22 +0500

    PORTFOLIO

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'Dashboard',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
+  },
+  {
+    title: 'Second page',
+    to: { name: 'second-page' },
+    icon: { icon: 'tabler-file' },
+  },
+]
